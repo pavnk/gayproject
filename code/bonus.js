cee1b613627844760839589a5e34be690440509e
Nikolas Pavlis <anakin956@gmail.com>
2019-05-10 16:37:18 +0200

    sceny lol

---
+++ code/bonus.js
@@ -0,0 +1,10 @@
+class Bonus extends GameObject {
+
+    constructor(game, x, y) {
+        super(game, x, y,"../obrazky/immunity.png");
+    }
+
+    update(dt) {
+        super.update(dt);
+    }
+}
--- code/hra.js
+++ code/hra.js
@@ -4,6 +4,17 @@ class Hra {
         this.ctx = this.canvas.getContext("2d");
         this.keys = [];
 
+        this.start();
+    }
+
+    start() {
+
+        this.menu = document.getElementById("main-menu");
+        this.gameNode = document.getElementById("game");
+        this.scoreNode = document.getElementById("score");
+        this.gameOverNode = document.getElementById("game-over");
+        this.gameOverScoreNode = document.getElementById("game-over-score");
+
         this.stage = new GameObject(this,0,0, null);
 
         this.bg = new Background();
@@ -27,14 +38,18 @@ class Hra {
             let prekazka = new Prekazka(this, 1500, this.vyska);
             this.stage.addChild(prekazka);
             this.prekazky.push(prekazka);
-
         };
 
-        this.running = true;
+        this.running = false;
+
+        document.addEventListener("click",() => {
+            this.running = true;
+            this.menu.style.display = "none";
+            this.gameNode.style.display = "block";
+            setInterval(this.handler, 2000);
+        }, {once: true});
 
         this.handler();
-
-        setInterval(this.handler, 2000)
     }
 
     onkeydown(event) {
@@ -45,23 +60,35 @@ class Hra {
     }
     update(dt){
 
-        if(!this.running) {
-            return;
-        }
+        if(this.running) {
 
-        for(let i of this.prekazky) {
-            if (GameObject.collision(this.player, i)) {
-                console.log("game over");
-                this.running = false;
+            for (let i of this.prekazky) {
+                if (GameObject.collision(this.player, i)) {
+                    this.running = false;
+                    this.gameNode.style.display = "none";
+                    this.gameOverNode.style.display = "block";
+                    this.gameOverScoreNode.innerHTML = this.score;
+                    clearInterval(this.handler);
+                    document.addEventListener("click", () => {
+
+                        this.start();
+                        this.gameOverNode.style.display = "none";
+
+                    }, {once:true});
+                    this.start();
+                }
             }
+
+            this.score++;
+            this.scoreNode.innerHTML = this.score;
+            if (this.score % 100 === 0) {
+                Prekazka.speed += 0.01;
+                console.log(Prekazka.speed);
+            }
+            this.stage.update(dt);
+            this.stage.draw(this.ctx);
+
         }
 
-        this.score++;
-        if(this.score % 100 === 0) {
-            Prekazka.speed += 0.01;
-            console.log(Prekazka.speed);
-        }
-        this.stage.update(dt);
-        this.stage.draw(this.ctx);
     }
 }
